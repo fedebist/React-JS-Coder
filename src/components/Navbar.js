3f8fe94d828f0522e5e10ad8fed36f6793f13d41
fedebist <fedebistolfi9@gmail.com>
2022-03-29 02:04:47 -0300

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import '../App.css';
+<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet"></link>
+
+let Navbar = () => {
+    return (
+      <nav class='navPersonalizado'>
+     <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Phone Store MDP</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Catálogo</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Nosotros</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    </nav>
+
+
+      
+);
+}
+
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+export default Navbar;
