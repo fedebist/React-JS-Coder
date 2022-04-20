84dccdb55b0537e56ba6501f68867c28a34dd8c1
fedebist <fedebistolfi9@gmail.com>
2022-04-20 01:15:43 -0300

    actualiz v2

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -2,13 +2,16 @@ import { useEffect, useState } from "react"
 import customFetch from "../utils/customFetch";
 import ItemDetail from './ItemDetail';
 const {products} = require('../data/Products');
+import { useParams } from 'react-router-dom';
 
 
 const ItemDetailContainer= () => {
     const [dato, setDato ] = useState({});
+    const { productId } = useParams();
 
     useEffect(()=> {
-        customFetch(2000, products[2])
+        customFetch(2000, products.find(item=> item.id
+            === parseInt( productId )))
         .then(result=> setDato(result))
         .catch(err=> console.log(err))
     }, []);
